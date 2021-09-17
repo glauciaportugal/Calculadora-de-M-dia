410475e51a854cbeda8072c47dd6102b4a9df2d2
glauciaportugal <88353946+glauciaportugal@users.noreply.github.com>
2021-09-17 14:11:26 -0300

    Add files via upload

---
+++ calculadora-de-m-dia/dist/script.js
@@ -0,0 +1,16 @@
+var nome = "Gláucia"
+var notaDoPrimeiroBimestre = 9.879
+var notaDoSegundoBimestre = 7.986
+var notaDoTerceiroBimestre = 4.09
+var notaDoQuartoBimestre = 2.03
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+var elementoNotaFinal = document.getElementById("notaFinal");
+  var notaFinal = "A sua média será " + notaFixada;
+  elementoNotaFinal.innerHTML = notaFinal;
